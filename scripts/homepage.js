8fb2cc49080f706c527bad0fc2f3e671cfb03802
N1Mirzo <mirzon1@umail.uz>
2020-01-03 20:51:40 +0500

    Uploaded for the firs time with some styles

---
+++ scripts/homepage.js
@@ -0,0 +1,10 @@
+/* Home Page JS */
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
